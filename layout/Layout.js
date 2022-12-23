6829f26b2cc5db6af1a4a8a4625b075d40490694
nbtheking <nhialbiellok@gmail.com>
2022-12-23 18:21:36 +0200

    some adjustment

--- layout/Layout.js
+++ layout/Layout.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Head from "next/head";
-import Navbar from "../components/navbar";
-// import Footer from "../components/footer";
+import Navbar from "../components/Navbar";
 
 const layout = ({ children }) => {
   return (
