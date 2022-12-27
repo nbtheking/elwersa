67eac578c79b8ffdb3cbf8ac5c8e6f20f5da575e
nbtheking <nhialbiellok@gmail.com>
2022-12-28 01:51:43 +0200

    adsense added

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,6 @@ import { Posts } from "../components/Post";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  <script
-    async
-    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3881651214880348"
-    crossorigin="anonymous"
-  ></script>;
   return (
     <div className={styles.container}>
       {Posts &&
