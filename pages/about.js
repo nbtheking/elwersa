22cb5b2f79232963741d8e650eece6ee8cafe1df
nbtheking <nhialbiellok@gmail.com>
2023-01-25 05:49:17 +0200

    class changed to className

--- pages/about.js
+++ pages/about.js
@@ -3,31 +3,31 @@ import Link from "next/link";
 
 const about = () => {
   return (
-    <div class=" pt-[150px] mx-10">
-      <div class="container mx-auto p-10 w-[100%] md:w-[700px] shadow-2xl bg-white">
-        <h1 class="text-2xl font-medium mb-4 text-center md:text-left">
+    <div className=" pt-[150px] mx-10">
+      <div className="container mx-auto p-10 w-[100%] md:w-[700px] shadow-2xl bg-white">
+        <h1 className="text-2xl font-medium mb-4 text-center md:text-left">
           About Elwersa
         </h1>
-        <p class="mb-4 text-center md:text-left">
+        <p className="mb-4 text-center md:text-left">
           Welcome to Elwersa, your go-to destination for all things cinema. We
           are dedicated to bringing you the latest and greatest in the world of
           movies. From the latest blockbuster releases to indie films you won't
           find anywhere else, we have something for everyone.
         </p>
-        <p class="mb-4 text-center md:text-left">
+        <p className="mb-4 text-center md:text-left">
           Our team of film enthusiasts scours the globe for the best movies, and
           we strive to provide you with in-depth reviews, trailers, and other
           information to help you make informed decisions about what to watch.
           We also provide showtimes and ticketing information for theaters near
           you.
         </p>
-        <p class="mb-4 text-center md:text-left">
+        <p className="mb-4 text-center md:text-left">
           In addition to the latest movie news, we also offer a wide variety of
           features, such as interviews with directors, actors, and other
           industry professionals, as well as retrospective pieces on classic
           films.
         </p>
-        <p class="mb-4 text-center md:text-left">
+        <p className="mb-4 text-center md:text-left">
           Thank you for visiting Elwersa, and we hope you enjoy exploring the
           world of cinema with us!
         </p>
--- pages/contact.js
+++ pages/contact.js
@@ -2,56 +2,56 @@ import React from "react";
 
 const contact = () => {
   return (
-    <div class="bg-gray-200 pt-[150px]">
-      <div class="container mx-auto px-4">
-        <h1 class="text-2xl font-medium mb-4 text-center">
+    <div className="bg-gray-200 pt-[150px]">
+      <div className="container mx-auto px-4">
+        <h1 className="text-2xl font-medium mb-4 text-center">
           Contact Us - Elwersa
         </h1>
-        <p class="text-center mb-4">
+        <p className="text-center mb-4">
           We'd love to hear from you! Whether you have a question about our
           website or a suggestion for a new feature, please don't hesitate to
           reach out.
         </p>
-        <form class="bg-white p-6 rounded-lg mb-4">
-          <div class="mb-4">
-            <label class="block text-gray-700 font-medium mb-2" for="name">
+        <form className="bg-white p-6 rounded-lg mb-4">
+          <div className="mb-4">
+            <label className="block text-gray-700 font-medium mb-2" for="name">
               Name
             </label>
             <input
-              class="border border-gray-400 p-2 w-full"
+              className="border border-gray-400 p-2 w-full"
               type="text"
               id="name"
               name="name"
               required
             />
           </div>
-          <div class="mb-4">
-            <label class="block text-gray-700 font-medium mb-2" for="email">
+          <div className="mb-4">
+            <label className="block text-gray-700 font-medium mb-2" for="email">
               Email
             </label>
             <input
-              class="border border-gray-400 p-2 w-full"
+              className="border border-gray-400 p-2 w-full"
               type="email"
               id="email"
               name="email"
               required
             />
           </div>
-          <div class="mb-4">
-            <label class="block text-gray-700 font-medium mb-2" for="message">
+          <div className="mb-4">
+            <label className="block text-gray-700 font-medium mb-2" for="message">
               Message
             </label>
             <textarea
-              class="border border-gray-400 p-2 w-full"
+              className="border border-gray-400 p-2 w-full"
               id="message"
               name="message"
             ></textarea>
           </div>
-          <button class="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600">
+          <button className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600">
             Submit
           </button>
         </form>
-        <p class="text-center">
+        <p className="text-center">
           You can also reach us at:{" "}
           <a href="mailto:info@elwersa.com">elwersa523@gmail.com.com</a> or{" "}
           <a href="tel:+201157133897">+201157133897</a>
